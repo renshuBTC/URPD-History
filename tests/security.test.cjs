585815bfbdf30dcd63025d3f39cf878599b85b8d
renshuBTC <renshutan5566@gmail.com>
2026-09-25 03:15:28 +0800

    Tests: light bar, font, palette

--- tests/security.test.cjs
+++ tests/security.test.cjs
@@ -55,7 +55,7 @@ test('the only download the page offers is the official .mp4 video, and its code
 
 test('every address in the page is one of the few it is meant to use', () => {
   const allowed = new Set([VIDEO_URL, PLOTLY, 'https://github.com/renshuBTC/URPD-History', 'https://bitview.space',
-    'https://fonts.googleapis.com', 'https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap',
+    'https://fonts.googleapis.com', 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap',
     'https://fonts.gstatic.com', 'http://www.w3.org/2000/svg', 'https://x.com/']);
   const found = [...html.matchAll(/https?:\/\/[^\s"'<>`)]+/g)].map(m => m[0].replace(/[;,.]+$/, ''));
   assert.ok(found.length >= allowed.size);
